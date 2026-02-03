45804e07e2fc81d930dd9ebacc5e4168835cfea4
gabriellakj08 <gabriellakj08@gmail.com>
2026-02-03 07:12:45 +0200

    Update main.py

--- Q3/main.py
+++ Q3/main.py
@@ -3,13 +3,12 @@ from dog import Dog
 from cat import Cat
 
 def main():
-    # יצירת אובייקטים ללא פרמטרים מיותרים
     animals = [Dog(), Cat()]
 
     for animal in animals:
-        # שימוש ב-isinstance כפי שנדרש במטלה
         if isinstance(animal, Animal):
             print(animal.speak())
 
 if __name__ == "__main__":
+
     main()
